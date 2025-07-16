c533ce6f367d2d02ab9df6e39fea2384c7b4f8f6
jennydev <jennydev2001@gmail.com>
2025-07-16 22:36:31 +0545

    footer changed

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     
   return (
     <div className="bg-gray-100 pt-1 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className='flex flex-col sm:grid grid-cols-[1.5fr_1fr_1fr] gap-14 my-10 mt-10 text-sm'>
             <div>
                 <img src={assets.logo} className='mb-5 w-32 ' />
                 <p className='w-full md:w-2/3 text-gray-600'>
