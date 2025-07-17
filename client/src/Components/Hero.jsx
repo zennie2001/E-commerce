7344945641446b9dcbe77d04e32b7b74a8a449b0
jennydev <jennydev2001@gmail.com>
2025-07-17 12:57:17 +0545

    banner changed

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,20 +1,88 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
+import banner1 from "../assets/banner1.png";
+import banner2nd from "../assets/banner2nd.png";
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md";
+
+const sliderImages = [
+  banner1,
+  banner2nd,
+  assets.banner_img, // third banner
+];
 
 function Hero() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Auto-slide every 4 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === sliderImages.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const goToPrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? sliderImages.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === sliderImages.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <div className='relative border  border-gray-400 rounded-md shadow-sm shadow-slate-300'>
-        {/*Hero Left side*/}
-        
+    <div className="relative border border-gray-400 rounded-md shadow-sm shadow-slate-300 overflow-hidden">
+      {/* Image Slide */}
+      <img
+        className="w-full lg:h-[450px] h-[250px] rounded-md object-cover transition duration-700 ease-in-out"
+        src={sliderImages[currentIndex]}
+        alt={`Banner ${currentIndex + 1}`}
+      />
 
-        {/* Hero Right Side */}
-        <img className='w-full lg:h-[450px] h-[300px] rounded-md object-cover' src={assets.banner_img} />
-        <Link to='/collection'><button  className='absolute bottom-8 left-1/2 transform -translate-x-1/2  px-4 py-2 border border-gray-400 rounded hover:bg-gray-400 hover:text-white'>Shop Now</button></Link>
+      {/* Shop Now button */}
+      <Link to="/collection">
+        <button className="absolute bottom-8 left-1/2 transform -translate-x-1/2 px-4 py-2 border border-gray-400 rounded hover:bg-gray-400 hover:text-white">
+          Shop Now
+        </button>
+      </Link>
 
+      {/* Left Arrow */}
+      <button
+        onClick={goToPrev}
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/80 text-black rounded-full p-2 hover:bg-white shadow"
+      >
+        <MdOutlineArrowBackIos size={20} />
+      </button>
 
+      {/* Right Arrow */}
+      <button
+        onClick={goToNext}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/80 text-black rounded-full p-2 hover:bg-white shadow"
+      >
+        <MdOutlineArrowForwardIos size={20} />
+      </button>
+
+      {/* Dots navigation */}
+      <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 flex gap-2">
+        {sliderImages.map((_, idx) => (
+          <span
+            key={idx}
+            onClick={() => setCurrentIndex(idx)}
+            className={`w-2 h-2 rounded-full cursor-pointer ${
+              currentIndex === idx ? 'bg-gray-800' : 'bg-gray-400'
+            }`}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,16 +4,27 @@ import LatestCollections from '../Components/LatestCollections'
 import BestSellers from '../Components/BestSellers'
 import OurPolicy from '../Components/OurPolicy'
 import NewsletterBox from '../Components/NewsletterBox'
+import { Helmet } from 'react-helmet';
 
 function Home() {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <>
+    <Helmet>
+        <title>Online Fashion E-Commerce Store | Forever Choice</title>
+        <meta 
+        name="description" 
+        content="Discover the latest trends in clothing, shoes, and accessories at Forever Choice. Shop quality fashion products online with fast delivery and secure checkout." />
+      </Helmet>
+
+       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[6vw]">
         <Hero />
         <LatestCollections />
        <BestSellers/>
        <OurPolicy/>
        
     </div>
+    </>
+   
   )
 }
 
