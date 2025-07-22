285bdd449a9c2b368b13e10f11308054691a051b
jennydev <jennydev2001@gmail.com>
2025-07-22 19:42:38 +0545

    Admin panel logo replaced

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -26,8 +26,8 @@ function Login({setToken}) {
 
   return (
     <div className='min-h-screen flex items-center justify-center w-full'>
-        <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md '>
-            <h1 className='text-2xl font-bold mb-4 '>Admin Panel</h1>
+        <div className='bg-white shadow-md rounded-lg px-8 py-10 max-w-md '>
+            <h1 className='text-2xl font-bold mb-12 text-center'>Admin Panel</h1>
             <form onSubmit={onSubmitHandler}>
                 <div className='mb-3 min-w-72'>
                     <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
