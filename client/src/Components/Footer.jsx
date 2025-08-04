cc6828a22823533620d20cefd0e024ee5a572ca2
jennydev <jennydev2001@gmail.com>
2025-08-04 22:02:09 +0545

    Footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -12,25 +12,25 @@ function Footer() {
         <div className='flex flex-col sm:grid grid-cols-[1.5fr_1fr_1fr] gap-14 my-10 mt-10 text-sm'>
             <div className='flex flex-col justify-start'>
                 <img src={assets.logo} className='mb-5 w-40 h-28 ' />
-                <p className='w-full md:w-2/3 text-gray-600 text-xl'>
+                <p className='w-full md:w-2/3 text-gray-900 text-xl'>
                     Foerver Choice Pvt.Ltd.
                 </p>
-                <p className='w-full md:w-2/3 text-gray-600 text-xl'>Kathmandu, Nepal</p>
+                <p className='w-full md:w-2/3 text-gray-900 text-xl'>Kathmandu, Nepal</p>
             </div>
 
             <div className='lg:pt-12'>
                 <p className='text-2xl font-medium mb-5'>COMPANY</p>
-                <ul className='flex flex-col gap-1 text-gray-600 text-lg '>
-                    <Link to='/' onClick={scrollToTop}><li className='hover:text-black '>Home</li></Link>
-                    <Link to='/about' onClick={scrollToTop}><li className='hover:text-black '>About Us</li></Link>
-                    <Link to='/contact'><li className='hover:text-black'>Contact Us</li></Link>
-                    <li className='hover:text-black '>Privacy Policy</li>
+                <ul className='flex flex-col gap-1 text-gray-900 text-lg '>
+                    <Link to='/' onClick={scrollToTop}><li className='hover:text-white'>Home</li></Link>
+                    <Link to='/about' onClick={scrollToTop}><li className='hover:text-white '>About Us</li></Link>
+                    <Link to='/contact'><li className='hover:text-white'>Contact Us</li></Link>
+                    <li className='hover:text-white '>Privacy Policy</li>
                 </ul>
             </div>
 
             <div className='pt-12'>
                 <p className='text-2xl font-medium mb-5'>CONNECT WITH US</p>
-                <ul className='flex flex-col gap-1 text-gray-600 text-lg'>
+                <ul className='flex flex-col gap-1 text-gray-900 text-lg'>
                     <li>+977-9800000000</li>
                     <li>contact@foreverchoice.com</li>
                    
